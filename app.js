cdeea93f6723fe1c83ee22eb1c87b72b87999729
sudo2801 <parag@basalanalytics.com>
2025-08-11 18:57:52 +0530

    wip

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }))
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://app-idreamcareer.desible.ai'
+  'https://ct-api-h0bzdsbvcbewcpae.centralus-01.azurewebsites.net'
 ]
 
 app.use(cors({
