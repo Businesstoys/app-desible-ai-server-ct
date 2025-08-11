30e1e3c8e5720d42e2582702e03d655afc1dc554
sudo2801 <parag@basalanalytics.com>
2025-08-11 19:14:20 +0530

    wip

--- app.js
+++ app.js
@@ -20,23 +20,15 @@ app.use(expressFileUpload())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://white-rock-01b058f10.2.azurestaticapps.net'
-]
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://white-rock-01b058f10.2.azurestaticapps.net'
+// ]
 
 app.use(cors({
   credentials: true,
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true)
-    if (
-      allowedOrigins.includes(origin) ||
-      /^https:\/\/.*\.desible\.ai$/.test(origin)
-    ) {
-      return callback(null, true)
-    } else {
-      return callback(new Error('Not allowed by CORS'))
-    }
+    callback(null, origin || true)
   }
 }))
 
