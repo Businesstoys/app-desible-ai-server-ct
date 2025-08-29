06acf3a7218d2c027f4d695e5cb21c4e4a8e5527
Varsha <varsha@basalAnalytics.com>
2025-08-29 11:19:13 +0530

    (#1)

--- models/statics/index.js
+++ models/statics/index.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose')
 
+const voiceSchema = new mongoose.Schema({
+  label: { type: String, required: true },
+  value: { type: String, required: true }
+}, { _id: false })
+
 const staticSchema = new mongoose.Schema({
   isQueueRunning: { type: Boolean, default: true },
   serverCapacity: { type: Number, default: 0 },
+  phoneNumbers: [{ type: String }],
+  voices: [voiceSchema],
+
+  prompt: { type: String },
+  selectedNumber: { type: String },
+  selectedVoice: { type: String },
 
   updatedOn: { type: Date, default: Date.now },
   createdOn: { type: Date, default: Date.now }
